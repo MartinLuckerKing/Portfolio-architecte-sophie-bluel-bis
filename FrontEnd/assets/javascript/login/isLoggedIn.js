9ec38f0591ed59034810fa860874a7491182b4ae
MartinLuckerKing <114108870+MartinLuckerKing@users.noreply.github.com>
2023-05-10 15:39:39 +0200

    isloggedIn display btn modifier

--- FrontEnd/assets/javascript/login/isLoggedIn.js
+++ FrontEnd/assets/javascript/login/isLoggedIn.js
@@ -3,11 +3,20 @@ export const isLoggedIn = () => {
         const token = localStorage.getItem('token');
         const login = document.querySelector("#login");
         const logout = document.querySelector("#logout");
+        const btn = document.querySelectorAll('.myBtn')
         
         if (token) {
             logout.style.display = 'block';
+            btn.forEach((button => {
+                button.style.display = 'block'
+            } ))
+            
+
         }
         else {
             login.style.display = 'block';
+            btn.forEach((button => {
+                button.style.display = 'none'
+            } ))
         }
   }
